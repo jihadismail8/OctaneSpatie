5b3bd1b7c5e11bd1d9b6a4c300a4a4088b8a4b1a
jihad ismail <jihad.ismail.8@gmail.com>
2024-07-11 10:39:40 +0300

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -17,13 +17,6 @@
                     </x-nav-link>
                 </div>
 
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('devices') }}" :active="request()->routeIs('devices')">
-                        {{ __('Devices') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Teams Dropdown -->
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
